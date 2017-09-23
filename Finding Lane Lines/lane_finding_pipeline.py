7aa48f22fb795738534c60e60dace9c1138c9796
Tran Ly Vu <vutransingapore@gmail.com>
2017-09-23 14:37:00 +0800

    add land finding project
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

---
+++ Finding Lane Lines/lane_finding_pipeline.py
@@ -0,0 +1,156 @@
+#importing some useful packages
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+import math
+import os
+
+os.listdir("test_images/")
+
+#reading in an image
+image = mpimg.imread('test_images/solidWhiteRight.jpg')
+
+#printing out some stats and plotting
+print('This image is:', type(image), 'with dimensions:', image.shape)
+plt.imshow(image)  # if you wanted to show a single color channel image called 'gray', for example, call as plt.imshow(gray, cmap='gray')
+
+
+
+def grayscale(img):
+    """Applies the Grayscale transform
+    This will return an image with only one color channel
+    but NOTE: to see the returned image as grayscale
+    (assuming your grayscaled image is called 'gray')
+    you should call plt.imshow(gray, cmap='gray')"""
+    return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Or use BGR2GRAY if you read an image with cv2.imread()
+    # return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+def canny(img, low_threshold, high_threshold):
+    """Applies the Canny transform"""
+    return cv2.Canny(img, low_threshold, high_threshold)
+
+def gaussian_blur(img, kernel_size):
+    """Applies a Gaussian Noise kernel"""
+    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+
+def region_of_interest(img, vertices):
+    """
+    Applies an image mask.
+    
+    Only keeps the region of the image defined by the polygon
+    formed from `vertices`. The rest of the image is set to black.
+    """
+    #defining a blank mask to start with
+    mask = np.zeros_like(img)   
+    
+    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+        
+    #filling pixels inside the polygon defined by "vertices" with the fill color    
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    
+    #returning the image only where mask pixels are nonzero
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+
+def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
+    """
+    NOTE: this is the function you might want to use as a starting point once you want to 
+    average/extrapolate the line segments you detect to map out the full
+    extent of the lane (going from the result shown in raw-lines-example.mp4
+    to that shown in P1_example.mp4).  
+    
+    Think about things like separating line segments by their 
+    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
+    line vs. the right line.  Then, you can average the position of each of 
+    the lines and extrapolate to the top and bottom of the lane.
+    
+    This function draws `lines` with `color` and `thickness`.    
+    Lines are drawn on the image inplace (mutates the image).
+    If you want to make the lines semi-transparent, think about combining
+    this function with the weighted_img() function below
+    """
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
+    """
+    `img` should be the output of a Canny transform.
+        
+    Returns an image with hough lines drawn.
+    """
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    draw_lines(line_img, lines)
+    return line_img
+
+# Python 3 has support for cool math symbols.
+
+def weighted_img(img, initial_img, α=0.8, β=1., λ=0.):
+    """
+    `img` is the output of the hough_lines(), An image with lines drawn on it.
+    Should be a blank image (all black) with lines drawn on it.
+    
+    `initial_img` should be the image before any processing.
+    
+    The result image is computed as follows:
+    
+    initial_img * α + img * β + λ
+    NOTE: initial_img and img must be the same shape!
+    """
+    return cv2.addWeighted(initial_img, α, img, β, λ)
+
+
+# TODO: Build your pipeline that will draw lane lines on the test_images
+# then save them to the test_images directory.
+solidWhiteCurve = mpimg.imread('test_images/solidWhiteCurve.jpg')
+
+# color selection
+
+
+#first grayscale the image
+solidWhiteCurve_gray = grayscale(solidWhiteCurve)
+
+#define a kernel size and apply gaussion smoothing
+kernel_size = 5
+solidWhiteCurve_gray_gaussian = gaussian_blur(solidWhiteCurve_gray, kernel_size)
+
+# Define our parameters for Canny and apply
+low_threshold = 50
+high_threshold = 150
+solidWhiteCurve_gray_gaussian_canny = canny(solidWhiteCurve_gray_gaussian, low_threshold, high_threshold)
+
+# This time we are defining a four sided polygon to mask
+imshape = image.shape
+vertices = np.array([[(0,imshape[0]),(450, 290), (490, 290), (imshape[1],imshape[0])]], dtype=np.int32)
+solidWhiteCurve_gray_gaussian_canny_masked = region_of_interest(solidWhiteCurve_gray_gaussian_canny, vertices)
+
+# Define the Hough transform parameters
+# Make a blank the same size as our image to draw on
+rho = 1                # distance resolution in pixels of the Hough grid
+theta = (np.pi)/180    # angular resolution in radians of the Hough grid
+threshold = 1          # minimum number of votes (intersections in Hough grid cell)
+min_line_len = 1       #minimum number of pixels making up a line
+max_line_gap = 50      # maximum gap in pixels between connectable line segments
+hough_lines = hough_lines(solidWhiteCurve_gray_gaussian_canny_masked, rho, theta, threshold, min_line_len, max_line_gap) 
+
+# Draw the lines on the edge image
+#lines_edges =  weighted_img(hough_lines, solidWhiteCurve, α=0.8, β=1., λ=0.) 
+cv2.imwrite('test_images_output/solidWhiteCurve.jpg', solidWhiteCurve)
+
+images = [solidWhiteCurve, 
+          solidWhiteCurve_gray, 
+          solidWhiteCurve_gray_gaussian, 
+          solidWhiteCurve_gray_gaussian_canny,
+         solidWhiteCurve_gray_gaussian_canny_masked]
+for ima in images:
+    plt.figure()
+    plt.imshow(ima)
--- canny.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import cv2
-
-image = mpimg.imread('exit-ramp.jpg')
-plt.imshow(image)
-gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-plt.imshow(gray, cmap='gray')
-edges = cv2.Canny(gray, low_threshold, high_threshold)
