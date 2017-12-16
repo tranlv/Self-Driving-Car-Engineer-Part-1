4054fc1ea081afa7ffd7e19af231ec5f19501de3
Tran Ly Vu <vutransingapore@gmail.com>
2017-12-16 15:24:54 +0700

    add notebook
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

--- Advanced Lane Lines/README.md
+++ Advanced Lane Lines/README.md
@@ -39,13 +39,15 @@ Writeup
 
 ### Camera Calibration and distortion correction
 
-The code for this step is contained in the 4th and 6th code cells of the IPython notebook located in "/notebook/advanced_lane_lines.ipynb" (or in lines # through # of the file called `some_file.py`).  
+The code for this step is contained in the 4th and 6th code cells of the IPython notebook located in "/notebook/advanced_lane_lines.ipynb" (or in lines 19 through 49 in /src/advanced_lane_lines.py).  
 
 I start by preparing "object points", which will be the (x, y, z) coordinates of the chessboard corners in the world. Here I am assuming the chessboard is fixed on the (x, y) plane at z=0, such that the object points are the same for each calibration image.  Thus, `objp` is just a replicated array of coordinates, and `objpoints` will be appended with a copy of it every time I successfully detect all chessboard corners in a test image.  `imgpoints` will be appended with the (x, y) pixel position of each of the corners in the image plane with each successful chessboard detection.  
 
-I then used the output `objpoints` and `imgpoints` to compute the camera calibration and distortion coefficients using the `cv2.calibrateCamera()` function.  I applied this distortion correction to the test image using the `cv2.undistort()` function and obtained this result: 
+I then used the output `obj_points` and `img_points` to compute the camera calibration and distortion coefficients using the `cv2.calibrateCamera()` function.  I applied this distortion correction to the test image using the `cv2.undistort()` function and obtained this result: 
 
-![alt text][image1]
+![alt text][https://github.com/tranlyvu/autonomous-vehicle-projects/blob/master/Advanced%20Lane%20Lines/camera_cal/calibration1.jpg]
+![alt text][https://github.com/tranlyvu/autonomous-vehicle-projects/blob/master/Advanced%20Lane%20Lines/output_images/undistort/undistort_calibration1.jpg]
+
 
 ### Pipeline (single images)
 
--- Advanced Lane Lines/src/advanced_lane_lines.py
+++ Advanced Lane Lines/src/advanced_lane_lines.py
@@ -12,32 +12,38 @@ CAMERA_IMAGE_DIRECTORY = '../camera_cal/'
 OUTPUT_DIRRECTORY = '../output_images/'
 TEST_IMAGES_DIRECTORY = '../test_images/'
 
-'''Define Parameters'''
-KERNEL_SIZE = 3
-OFFSET = 100
-NX = 9
-NY = 6
-THRESHOLD_MIN = 0
-THRESHOLD_MAX = 255
-MAGNITUDE_THRESHOLD = (0, 255)
-DIRECTION_THRESHOLD = (0, np.pi/2)
-SATURATION_THRESHOLD = (170, 255)
+'''Read images directories'''
+chessboard_images = glob.glob(CAMERA_IMAGE_DIRECTORY + 'calibration*.jpg')
+test_images = glob.glob(TEST_IMAGES_DIRECTORY + '*.jpg')
 
 #Prepare obj poinrts
-objp = np.zeros((NX * NY, 3), np.float32)
-objp[:,:2] = np.mgrid[0:NX, 0:NY].T.reshape(-1,2)
+objp = np.zeros((9 * 6, 3), np.float32)
+objp[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1,2)
 obj_points = [] #3d points in real world space
 img_points = [] #2d points in image points
 
-images = glob.glob(CAMERA_IMAGE_DIRECTORY + 'calibration*.jpg')
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-for idx, fname in enumerate(images):
+for i, fname in enumerate(chessboard_images):
     img = cv2.imread(fname)
     image_name=os.path.split(fname)[1]
-    ret, corners = cv2.findChessboardCorners(img, (NX, NY), None)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    
+    ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
     if ret == True:
         obj_points.append(objp)
-        img_points.append(corners)
-        cv2.drawChessboardCorners(img, (NX, NY), corners, ret)
-        corners_write_name = OUTPUT_DIRRECTORY + 'chessboard_corners/' +  'corners_found_'+ str(NX) + '_' + str(NY) + '_' + image_name
+        
+        # this step to refine image points was taken from:
+        # http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_calib3d/py_calibration/py_calibration.html
+        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        img_points.append(corners2)
+        
+        cv2.drawChessboardCorners(img, (9, 6), corners, ret)
+        corners_write_name = OUTPUT_DIRRECTORY + 'chessboard_corners/' +  'corners_found_'+ str(9) + '_' + str(6) + '_' + image_name
         cv2.imwrite(corners_write_name, img)
+
+# Calcualte the undistortion matrix to calculate matrix and distance coefficient
+img = cv2.imread(chessboard_images[0])
+img_size = (img.shape[1], img.shape[0])
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, img_size, None, None)
