77f0dbf6f13b49b081ba7af7b4546ab1e755d424
Tran Ly Vu <vutransingapore@gmail.com>
2017-12-27 13:54:50 +0700

    findish project 1
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

--- Traffic Sign Classifier/README.md
+++ Traffic Sign Classifier/README.md
@@ -164,22 +164,32 @@ Here are the results of the prediction:
 
 The model was able to correctly guess 1 of the 5 traffic signs, which gives an accuracy of 20%. This does not correspond to the accuracy on the test set.
 
-#### Describe how certain the model is when predicting on each of the five new images by looking at the softmax probabilities for each prediction. Provide the top 5 softmax probabilities for each image along with the sign type of each probability. (OPTIONAL: as described in the "Stand Out Suggestions" part of the rubric, visualizations can also be provided such as bar charts)
+#### Describe how certain the model is when predicting on each of the five new images by looking at the softmax probabilities for each prediction. Provide the top 5 softmax probabilities for each image along with the sign type of each probability. 
 
-The code for making predictions on my final model is located in the 11th cell of the Ipython notebook.
+For the first image, the model's first choice was no vehicle sign (0.93) while the correct sign was third rank (0.026)
 
-For the first image, the model is relatively sure that this is a stop sign (probability of 0.6), and the image does contain a stop sign. The top five soft max probabilities were
-
-| Probability         	|     Prediction	        					| 
-|:---------------------:|:---------------------------------------------:| 
-| .60         			| Stop sign   									| 
-| .20     				| U-turn 										|
-| .05					| Yield											|
-| .04	      			| Bumpy Road					 				|
-| .01				    | Slippery Road      							|
+| Prediction			        	  |     Probability	             | 
+|:---------------------------:|:-----------------------------:| 
+| Road work     			  | 0.93					 | 
+| Traffic signals  			  | 0.038 					 |
+| Stop sign	                  | 0.026							     |
+| Keep right      		      | 0.00165					 			 |
+| Bumpy road		          | 0.0013 |
 
 
-For the second image ... 
+The model predicted correctly the second image - the Yield sign (almost 1)
+
+| Prediction			        	  |     Probability	             | 
+|:---------------------------:|:-----------------------------:| 
+| Yield Sign     			  | ~1					 | 
+| Children crossing           | ~0 					 |
+| End of all speed and passing limits | ~0							     |
+| Speed limit (100km/h)     | ~0					 			 |
+| Priority road             | ~0 |
+
+Other images can be seen from the notebook
+
+
 
 ### (Optional) Visualizing the Neural Network (See Step 4 of the Ipython notebook for more details)
 ####1. Discuss the visual output of your trained network's feature maps. What characteristics did the neural network use to make classifications?
--- Traffic Sign Classifier/src/second_attempt.py
+++ Traffic Sign Classifier/src/second_attempt.py
@@ -1,46 +1,16 @@
-# Load pickled data
 import pickle
 import pandas as pd
 import numpy as np
+import random
 import cv2
-from tensorflow.contrib.layers import flatten
+import glob
 import tensorflow as tf
+from tensorflow.contrib.layers import flatten
+from tensorflow.contrib.layers import flatten
 from sklearn.utils import shuffle
 
 
-'''Parameters setting'''
-MEAN = 0.0
-SIGMA = 0.1
-EPOCHS = 80
-BATCH_SIZE = 128
-LEARNING_RATE = 0.0001
-
-''' Pre-processing pipeline 
-    - graysclale 
-    - normalize
-    - reshape input data to (32,32,1)
-''' 
-
-# The data is probably in order RGB
-# type uint8
-training_file = '../../../train.p'
-validation_file= '../../../valid.p'
-testing_file = '../../../test.p'
-
-with open(training_file, mode='rb') as f:
-    train = pickle.load(f)
-with open(validation_file, mode='rb') as f:
-    valid = pickle.load(f)
-with open(testing_file, mode='rb') as f:
-    test = pickle.load(f)
-    
-X_train_original, y_train_original = train['features'], train['labels']
-X_valid_original, y_valid_original = valid['features'], valid['labels']
-X_test_original, y_test_original = test['features'], test['labels']
-
-'''Helper functions'''
 def grayscale(input_image):
-    """grayscale"""
     output = []
     for i in range(len(input_image)): 
         img = cv2.cvtColor(input_image[i], cv2.COLOR_RGB2GRAY)
@@ -60,8 +30,7 @@ def normalization(input_image):
     return output
 
 def get_weights(input_shape):
-    return tf.Variable(tf.truncated_normal(shape = input_shape, 
-                                           mean = MEAN, stddev = SIGMA))
+    return tf.Variable(tf.truncated_normal(shape = input_shape, mean = 0.0, stddev = 0.1))
 
 def get_biases(length):
      return tf.Variable(tf.zeros(length))
@@ -104,6 +73,10 @@ def fully_connected_layer(input_layer,
     layer = tf.matmul(input_layer, weights) + biases
     return layer
 
+def dropout_layer(layer, keep_prob):
+    layer = tf.nn.dropout(layer, keep_prob)
+    return layer
+
 """Pre-processing data"""
 
 def pre_process_second_attempt(input_image):
@@ -115,15 +88,26 @@ def pre_process_second_attempt(input_image):
 X_train_final_2 = pre_process_second_attempt(X_train_original) 
 X_valid_final_2 = pre_process_second_attempt(X_valid_original)
 
+"""Pre-processing data"""
+def preprocess_data(input_image):
+    gray_image = grayscale(input_image)
+    output =  normalization(gray_image)
+    output = np.expand_dims(output, 3)
+    return output
+
+X_train_final = preprocess_data(X_train_original) 
+X_valid_final = preprocess_data(X_valid_original)
+print(X_train_final[0].shape)
+
 """Model design"""
-def Lenet_5_second_attempt(input_image):    
+def Lenet_5_model(input_image):    
     
     # Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x10.
     conv1 = convolution_layer(input_image, 5, 1, 10, 'VALID')
     conv1 = activation_relu(conv1)
     
     # Layer 2: Convolutional. Input = 28x28x10. Output = 24x24x20.
-    conv2 = convolution_layer(conv2, 5, 10, 20, 'VALID')
+    conv2 = convolution_layer(conv1, 5, 10, 20, 'VALID')
     conv2 = activation_relu(conv2)
     # drop-out
     conv2 = dropout_layer(conv2, keep_prob)
@@ -136,14 +120,14 @@ def Lenet_5_second_attempt(input_image):
     
     # Layer 4: Convolutional. Input = 20x20x30. Output = 16x16x40.   
     conv4 = convolution_layer(conv3, 5, 30, 40, 'VALID')
-    conv4 = tf.nn.relu(conv2)
+    conv4 = tf.nn.relu(conv4)
     # max_pool: output = 8x8x40
-    conv4 = max_spooling(conv2, 'VALID')
+    conv4 = max_spooling(conv4, 'VALID')
     # drop-out
     conv4 = dropout_layer(conv4, keep_prob)
 
     # Flatten. Input = 8x8x40. Output = 2560.
-    fc0   = flatten_layer(conv2)        
+    fc0   = flatten_layer(conv4)        
     
     # Layer 5: Fully Connected. Input = 2560. Output = 1280.
     fc1 = fully_connected_layer(fc0, 2560, 1280)
@@ -166,11 +150,12 @@ def Lenet_5_second_attempt(input_image):
     fc5 = tf.nn.relu(fc5)
     
     # Layer 10: Fully Connected. Input = 80. Output = 43 
-    logits = fully_connected_layer(fc2, 80, 43)   
+    logits = fully_connected_layer(fc5, 80, 43)   
     
     return logits
 
-def evaluate_second_attempt(X_data, y_data, keep_prob):
+"""Evaluation function"""
+def evaluate(X_data, y_data, my_keep_prob):
     num_examples = len(X_data)
     total_accuracy = 0
     total_loss = 0
@@ -179,7 +164,7 @@ def evaluate_second_attempt(X_data, y_data, keep_prob):
         batch_x, batch_y = X_data[offset : offset + BATCH_SIZE], y_data[offset : offset + BATCH_SIZE]
         loss, accuracy = sess.run([loss_operation, accuracy_operation], feed_dict={x: batch_x, 
                                                                                    y: batch_y, 
-                                                                                   keep_prob: keep_prob})
+                                                                                   keep_prob: my_keep_prob})
         total_accuracy += (accuracy * len(batch_x))
         total_loss += (loss * len(batch_x))
     return total_loss / num_examples, total_accuracy / num_examples 
@@ -188,56 +173,82 @@ def evaluate_second_attempt(X_data, y_data, keep_prob):
 """Training data"""
 if __name__ == "__main__":
 
+  ''' Pre-processing pipeline 
+      - graysclale 
+      - normalize
+      - reshape input data to (32,32,1)
+  ''' 
+
+  # The data is probably in order RGB
+  # type uint8
+  training_file = '../../../train.p'
+  validation_file= '../../../valid.p'
+  testing_file = '../../../test.p'
+
+  with open(training_file, mode='rb') as f:
+      train = pickle.load(f)
+  with open(validation_file, mode='rb') as f:
+      valid = pickle.load(f)
+  with open(testing_file, mode='rb') as f:
+      test = pickle.load(f)
+      
+  X_train_original, y_train_original = train['features'], train['labels']
+  X_valid_original, y_valid_original = valid['features'], valid['labels']
+  X_test_original, y_test_original = test['features'], test['labels']
+
+
+    """Parameters setting"""
+  EPOCHS = 40
+  BATCH_SIZE = 128
+  LEARNING_RATE = 0.0001
+
+  '''Training and save'''
+
   keep_prob = tf.placeholder(tf.float32)
+  # x is a placeholder for a batch of input images. y is a placeholder for a batch of output labels.
   x = tf.placeholder(tf.float32, (None, 32, 32, 1))
   y = tf.placeholder(tf.int32, (None))
+  # convert to 1 hot-coded data
   one_hot_y = tf.one_hot(y, 43)
-  logits = Lenet_5_second_attempt(x)
+
+  logits = Lenet_5_model(x)
+
   cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
   loss_operation = tf.reduce_mean(cross_entropy)
   optimizer = tf.train.AdamOptimizer(learning_rate = LEARNING_RATE)
   training_operation = optimizer.minimize(loss_operation)
+
   correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
   accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
   saver = tf.train.Saver()
 
   train_loss_history = []
   valid_loss_history = []
 
+
   #Start running tensor flow
   with tf.Session() as sess:
-    training(Lenet_5_first_attempt)
-    sess.run(tf.global_variables_initializer())
-    num_examples = len(X_train_final_1)
+      sess.run(tf.global_variables_initializer())
+      num_examples = len(X_train_final)
 
-    print("Training...")
-    print()
-    for i in range(EPOCHS):
-        X_train, y_train = shuffle(X_train_final_1, y_train_original)
-        for offset in range(0, num_examples, BATCH_SIZE):
-            end = offset + BATCH_SIZE
-            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
-            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 0.5})
+      print("Training...")
+      for i in range(EPOCHS):
+          X_train, y_train = shuffle(X_train_final, y_train_original)
+          for offset in range(0, num_examples, BATCH_SIZE):
+              end = offset + BATCH_SIZE
+              batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+              sess.run(training_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 0.5})
 
-        valid_loss, valid_accuracy = evaluate(X_valid_final_1, y_valid_original, 1.0)
-        valid_loss_history.append(valid_loss)
+          valid_loss, valid_accuracy = evaluate(X_valid_final, y_valid_original, 1.0)
+          valid_loss_history.append(valid_loss)
 
-        train_loss, train_accuracy = evaluate(X_train_final_1, y_train_original, 1.0)
-        train_loss_history.append(train_loss)
+          train_loss, train_accuracy = evaluate(X_train_final, y_train_original, 1.0)
+          train_loss_history.append(train_loss)
 
-        print("EPOCH {} ...".format(i+1))
-        print("Training Accuracy = {:.3f}".format(train_accuracy))
-        print("Validation Accuracy = {:.3f}".format(valid_accuracy))
+          print("EPOCH {} ...".format(i + 1))
+          print("Training Accuracy = {:.3f}".format(train_accuracy))
+          print("Validation Accuracy = {:.3f}".format(valid_accuracy))
 
-        print("Training Loss = {:.3f}".format(train_loss))
-        print("Validation Loss = {:.3f}".format(valid_loss))
-
-
-    saver.save(sess, './lenet')
-    print("Model saved")
-    loss_plot = plt.subplot(2,1,1)
-    loss_plot.set_title('Loss')
-    loss_plot.plot(train_loss_history, 'r', label='Training Loss')
-    loss_plot.plot(valid_loss_history, 'b', label='Validation Loss')
-    loss_plot.set_xlim([0, EPOCHS])
-    loss_plot.legend(loc=4)
+          print("Training Loss = {:.3f}".format(train_loss))
+          print("Validation Loss = {:.3f}".format(valid_loss))
