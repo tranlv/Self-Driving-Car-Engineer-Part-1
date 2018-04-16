d0d93596036031edf493b2035a6e30a677ae64ac
Tran Ly Vu <vutransingapore@gmail.com>
2018-04-16 17:11:53 +0800

    Update project PID Control
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

--- PID Control/src/main.cpp
+++ PID Control/src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(0.2, 0.004, 3.0)
+  pid.Init(0.2, 0.004, 3.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -58,8 +58,8 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          pid.UpdateError(cte)
-          steer_value = pid.TotalError()
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
